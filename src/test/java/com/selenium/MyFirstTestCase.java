45af4fd793db947157b108ece12272b479b59848
“rajatt95” <rajatvermaa95@gmail.com>
2022-10-01 01:59:07 +0530

    Section 3 (Setup) -> Setup Project using IntelliJ - Done

---
+++ src/test/java/com/selenium/MyFirstTestCase.java
@@ -0,0 +1,12 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MyFirstTestCase {
+
+    @Test
+    public void dummyTestCase(){
+        WebDriver driver = new ChromeDriver();
+    }}
